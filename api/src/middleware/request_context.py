17119ca8978b6057a73ecb25d899f3d2d93f18fb
Hoid <tylerlcheek@gmail.com>
2026-03-13 14:08:26 -0400

    Fix import issue causing typecheck failure

--- api/src/middleware/request_context.py
+++ api/src/middleware/request_context.py
@@ -7,7 +7,7 @@ from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.types import ASGIApp
 
-from src.core.logging import request_id_context
+from core.logging import request_id_context
 
 logger = logging.getLogger(__name__)
 
