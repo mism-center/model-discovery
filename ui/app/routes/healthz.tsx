0d2b6de5139f4007ac984b51e8ac4ae514519144
Hoid <tylerlcheek@gmail.com>
2026-05-22 16:34:38 -0400

    Add /healthz route to the UI

---
+++ ui/app/routes/healthz.tsx
@@ -0,0 +1,9 @@
+export function loader() {
+  return new Response('OK', {
+    status: 200,
+    headers: {
+      'Cache-Control': 'no-store',
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  });
+}
