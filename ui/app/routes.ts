5a12cb37a1b1bfb53f9975bf7dbc1749b98a749f
Hoid <tylerlcheek@gmail.com>
2026-05-22 16:37:28 -0400

    Add forgotten change to routes.ts

--- ui/app/routes.ts
+++ ui/app/routes.ts
@@ -4,4 +4,5 @@ export default [
   index('routes/index.tsx'),
   route('search', 'routes/search.tsx'),
   route('tus-test', 'routes/tus-test.tsx'),
+  route('healthz', 'routes/healthz.tsx'),
 ] satisfies RouteConfig;
